c0d60a2229b6a18878c67964c94b8d066f8d8546
SergeyMikhalkin <sergey.mikhalkin.work@gmail.com>
2022-05-21 18:30:22 +0300

    feat: implement task 04-copy-directory (Sat, May 21, 2022 06:30:22 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
+const path = require('path');
 
+const pathToFolder = path.join(__dirname, 'files');
+const pathToFolderCopy = path.join(__dirname, 'files-copy'); 
+
+fs.promises.mkdir(pathToFolderCopy, {recursive: true}).then(() => {
+    fs.promises.readdir(pathToFolder, { withFileTypes: true }).then((files) => {
+        files.forEach(file => {        
+            if(file.isFile()) {
+                let src = path.join(pathToFolder, file.name);
+                let dest = path.join(pathToFolderCopy, file.name);
+                fs.promises.copyFile(src, dest);
+            };
+        });
+    }).catch((err) => {
+        console.log(err, 'Failed to read directory');
+    });
+    
+}).catch((err) => {
+    console.log(err, 'Failed to create directory');
+});
